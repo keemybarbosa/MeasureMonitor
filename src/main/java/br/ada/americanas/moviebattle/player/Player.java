c232fcd9cfd1d5d676d05dc36ec3a0903e2c601a
William Cesar Santos <william_cesar_santos@hotmail.com>
2023-03-06 21:23:10 -0300

    Update

---
+++ src/main/java/br/ada/americanas/moviebattle/player/Player.java
@@ -0,0 +1,32 @@
+package br.ada.americanas.moviebattle.player;
+
+public class Player {
+
+    private Long id;
+    private String name;
+    private Float score;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getScore() {
+        return score;
+    }
+
+    public void setScore(Float score) {
+        this.score = score;
+    }
+}
