0832afafa905a518933a2ffe5c5405a76993bc51
William Cesar Santos <william_cesar_santos@hotmail.com>
2023-03-08 21:37:46 -0300

    Update

--- src/main/java/br/ada/americanas/moviebattle/movie/MovieAppController.java
+++ src/main/java/br/ada/americanas/moviebattle/movie/MovieAppController.java
@@ -3,11 +3,11 @@ package br.ada.americanas.moviebattle.movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller web para atender as requisições. Não segue o padrão REST
+ */
 @Controller
 @RequestMapping("/app/movies")
 public class MovieAppController {
@@ -27,8 +27,28 @@ public class MovieAppController {
 
     @GetMapping("/create")
     public String create(Model model) {
+        // Fornece acesso a página html que representa o formulário. O cadastro acontece pelo POST
         model.addAttribute("movie", new Movie());
         return "movie/form";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(
+            @PathVariable("id") Long id,
+            Model model
+    ) {
+        Movie movie = service.findById(id).get();
+        model.addAttribute("movie", movie);
+        return "movie/form";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(
+            @PathVariable("id") Long id,
+            Model model
+    ) {
+        service.delete(id);
+        return "redirect:/app/movies";
     }
 
     @PostMapping
